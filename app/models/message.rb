a2a63da7606f8a46ca61f806de23d976606f3f99
Austin Steward & Sean Morrison <student@epicodus.com>
2014-09-08 16:04:35 -0700

    Add mass texting :hocho:

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,16 +2,18 @@ class Message < ActiveRecord::Base
   before_save :send_message
 
 private
-
   def send_message
-    response = RestClient::Request.new(
-      :method => :post,
-      :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
-      :user => ENV['TWILIO_ACCOUNT_SID'],
-      :password => ENV['TWILIO_AUTH_TOKEN'],
-      :payload => { :Body => body,
-                    :To => to,
-                    :From => from }
-    ).execute
+    numbers = to.split(",")
+    numbers.each do |number|
+      response = RestClient::Request.new(
+        :method => :post,
+        :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
+        :user => ENV['TWILIO_ACCOUNT_SID'],
+        :password => ENV['TWILIO_AUTH_TOKEN'],
+        :payload => { :Body => body,
+                      :To => number,
+                      :From => from }
+      ).execute
+    end
   end
 end
